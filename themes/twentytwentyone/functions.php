4d5b13caf01e10c0cb5890c05f276e10fbb34227
leochen <leo000111444@gmail.com>
2023-07-28 20:12:45 +0800

    adjust style & add subpage function

--- themes/twentytwentyone/functions.php
+++ themes/twentytwentyone/functions.php
@@ -641,6 +641,31 @@ function twentytwentyone_add_ie_class() {
 }
 add_action( 'wp_footer', 'twentytwentyone_add_ie_class' );
 
+
+// child page of main menu
+function wpb_list_child_pages() { 
+  
+global $post; 
+  
+if ( is_page() && $post->post_parent )
+  
+    $childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->post_parent . '&echo=0' );
+else
+    $childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0' );
+  
+if ( $childpages ) {
+  
+    $string = '<ul class="wpb_page_list">' . $childpages . '</ul>';
+}
+  
+return $string;
+  
+}
+  
+add_shortcode('wpb_childpages', 'wpb_list_child_pages');
+
+
+
 if ( ! function_exists( 'wp_get_list_item_separator' ) ) :
 	/**
 	 * Retrieves the list item separator based on the locale.
